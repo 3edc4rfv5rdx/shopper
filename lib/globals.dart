06dde12087326dd150abb62fb363cf70fd5ef99e
e <e@e.ee>
2026-02-23 11:21:51 +0200

    Version bump to 0.8.260223+37: remove warnings etc

--- lib/globals.dart
+++ lib/globals.dart
@@ -7,6 +7,6 @@ export 'globals-ui-helpers.dart';
 export 'globals-item-dialog.dart';
 
 const String progVersion = '0.8.260223';
-const int buildNumber = 36;
+const int buildNumber = 37;
 const String progAuthor = 'Eugen';
 bool xvDebug = true;
