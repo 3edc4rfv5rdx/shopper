768efe8a424f522fca1d336f0396618fbdc5a364
e <e@e.ee>
2026-03-13 11:21:47 +0200

    Version bump to 0.8.260313+48: new build

--- lib/globals.dart
+++ lib/globals.dart
@@ -7,6 +7,6 @@ export 'globals-ui-helpers.dart';
 export 'globals-item-dialog.dart';
 
 const String progVersion = '0.8.260313';
-const int buildNumber = 47;
+const int buildNumber = 48;
 const String progAuthor = 'Eugen';
 bool xvDebug = true;
